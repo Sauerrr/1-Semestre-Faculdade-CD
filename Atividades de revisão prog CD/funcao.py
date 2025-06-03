16c35590c0a7aed981731548550a06d85dfb6a72
Sauerrr <sauervitor12@gmail.com>
2025-06-03 19:57:55 -0300

    Criado apenas um codigo para função em Python

---
+++ Atividades de revisão prog CD/funcao.py
@@ -0,0 +1,7 @@
+def soma(a, b):
+    return a + b
+
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+resultado = soma(a, b)
+print("A soma é:", resultado)   
