01210f9295d87cf9d79e140f53952f64ffea0f3b
Sauerrr <sauervitor12@gmail.com>
2025-06-03 21:21:16 -0300

    update

--- Atividades de revisão prog CD/funcao.py
+++ Atividades de revisão prog CD/funcao.py
@@ -4,4 +4,4 @@ def soma(a, b):
 a = float(input("Digite o primeiro número: "))
 b = float(input("Digite o segundo número: "))
 resultado = soma(a, b)
-print("A soma é:", resultado)   
+print("A soma é:", resultado)
